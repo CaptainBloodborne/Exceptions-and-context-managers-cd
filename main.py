7d59be44fb23eaa2fa67b08daa7490a93e34c89f
artem <vrail30@outlook.com>
2021-11-03 17:48:40 +0300

    Task done.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import os
+
+
 class Cd:
-    pass
+    current_dir = os.getcwd()
+
+    def __init__(self, path):
+        if not os.path.isdir(path):
+            raise ValueError
+        self.path = path
+
+    def __enter__(self):
+        os.chdir(self.path)
+        print(f"Working directory is changed to {self.path}")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(f"Working directory is changed to {self.current_dir}")
